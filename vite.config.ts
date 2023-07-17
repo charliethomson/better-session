fca3bac7d7ed6491cb9204e771ade0467a7148b9
Charlie Thomson <charliecthomson@gmail.com>
2023-07-16 20:44:56 -0400

    cleanup + setup auto build + release

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom"
-  }
+    plugins: [dts(), react()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/lib/index.ts"),
+            name: "better-session",
+            fileName: "better-session",
+        },
+    },
+    test: {
+        environment: "jsdom"
+    }
 })
